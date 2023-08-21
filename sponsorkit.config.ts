fdd6303ece078f3db847498b584ce42c84f52050
Mirone <Saul-Mirone@outlook.com>
2023-08-21 20:21:37 +0800

    chore: update config

--- sponsorkit.config.ts
+++ sponsorkit.config.ts
@@ -1,8 +1,37 @@
-import { defineConfig } from "sponsorkit";
+import { defineConfig, presets } from "sponsorkit";
 
 export default defineConfig({
   github: {
     login: "Saul-Mirone",
     type: "user",
   },
+  formats: ['svg'],
+  tiers: [
+    // Past sponsors, currently only supports GitHub
+    {
+      title: 'Past Sponsors',
+      monthlyDollars: -1,
+      preset: presets.xs,
+    },
+    // Default tier
+    {
+      title: 'Backers',
+      preset: presets.base,
+    },
+    {
+      title: 'Sponsors',
+      monthlyDollars: 10,
+      preset: presets.medium,
+    },
+    {
+      title: 'Silver Sponsors',
+      monthlyDollars: 50,
+      preset: presets.large,
+    },
+    {
+      title: 'Gold Sponsors',
+      monthlyDollars: 100,
+      preset: presets.xl,
+    },
+  ],
 });
